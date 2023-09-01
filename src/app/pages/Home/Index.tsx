3d4328eb7cb234c298d696bfd043d909282c9a95
Fuad-Imanov <imanovfuad55@gmail.com>
2023-09-01 16:10:27 +0400

    mesaj

--- src/app/pages/Home/Index.tsx
+++ src/app/pages/Home/Index.tsx
@@ -12,68 +12,61 @@ import logo from '../Home/images/Vector.svg'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
+      
+    <div className="carousel-wrapper">
+        <ReactOwlCarousel
+          items={1}
+          className='owl-theme'
+          autoPlay
+          nav
+          dots
+          loop><div className="slide">
+            <div className="content">
+              <div className="text-container">
+                <div className="text-content">
+                  <span className='span'>WELCOME TO GREENSHOP</span>
+                  <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
+                  <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
+                  <a href="#" className="btn">SHOP NOW</a>
+                </div>
+                <div className="images">
+                  <img src={images} alt={`Imags`} />
+                </div>
+              </div>
+            </div>
+          </div> <div className="slide">
+            <div className="content">
+              <div className="text-container">
+                <div className="text-content">
+                  <span className='span'>WELCOME TO GREENSHOP</span>
+                  <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
+                  <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
+                  <a href="#" className="btn">SHOP NOW</a>
+                </div>
+                <div className="images">
+                  <img src={images} alt={`Imags`} />
+                </div>
+              </div>
+            </div>
+          </div> <div className="slide">
+            <div className="content">
+              <div className="text-container">
+                <div className="text-content">
+                  <span className='span'>WELCOME TO GREENSHOP</span>
+                  <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
+                  <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
+                  <a href="#" className="btn">SHOP NOW</a>
+                </div>
+                <div className="images">
+                  <img src={images} alt={`Imags`} />
+                </div>
+              </div>
+            </div>
+          </div></ReactOwlCarousel> </div></>)
 
-    </div>
-  );
-}
+  ;
+  }
 
-
-function Car() {
-  return (<div className="carousel-wrapper">
-    <ReactOwlCarousel
-      items={1}
-      className='owl-theme'
-      autoPlay
-      nav
-      dots
-      loop><div className="slide">
-      <div className="content">
-        <div className="text-container">
-          <div className="text-content"> 
-            <span className='span'>WELCOME TO GREENSHOP</span>
-            <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
-            <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
-            <a href="#" className="btn">SHOP NOW</a>
-          </div>
-          <div className="images">
-            <img src={images} alt={`Imags`} />
-          </div>
-        </div>
-      </div>
-    </div> <div className="slide">
-      <div className="content">
-        <div className="text-container"> 
-          <div className="text-content"> 
-            <span className='span'>WELCOME TO GREENSHOP</span>
-            <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
-            <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
-            <a href="#" className="btn">SHOP NOW</a>
-          </div>
-          <div className="images">
-            <img src={images} alt={`Imags`} />
-          </div>
-        </div>
-      </div>
-    </div> <div className="slide">
-      <div className="content">
-        <div className="text-container"> 
-          <div className="text-content"> 
-            <span className='span'>WELCOME TO GREENSHOP</span>
-            <h1>{"LET'S MAKE A BETTER PLANET"}</h1>
-            <p>{"We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create a unique Urban Jungle. Order your favorite plants!"}</p>
-            <a href="#" className="btn">SHOP NOW</a>
-          </div>
-          <div className="images">
-            <img src={images} alt={`Imags`} />
-          </div>
-        </div>
-      </div>
-    </div></ReactOwlCarousel> </div>)
-}
-
-
-const app = ReactDOM.createRoot(document.getElementById("carousel")!)
-app.render(<Car></Car>)
 export default Home;
